21e82e2fe938bc8f97739fc157d56c703cbf44cf
micarollo <rollomicaela@gmail.com>
2024-04-10 06:25:02 -0400

    mariadb ok

--- srcs/requeriments/mariadb/tools/mariadb.sh
+++ srcs/requeriments/mariadb/tools/mariadb.sh
@@ -8,8 +8,9 @@ then
 	mysql -u root -e "CREATE DATABASE $DB_NAME;"
 	mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 	mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
+	#mysql -u root -e "FLUSH PRIVILEGES;"
+	mysql -u root -e "CREATE USER '$DB_ROOT_USER'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
-	mysql -u root -e "ALTER USER '$DB_ROOT_USER'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
 fi
 
 mysqld
--- srcs/requeriments/wordpress/tools/wp-script.sh
+++ srcs/requeriments/wordpress/tools/wp-script.sh
@@ -5,7 +5,7 @@ then
 	cd /var/www/html
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
-	wp core install --url=$WP_DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --allow-root
+	wp core install --url=$WP_DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=editor --allow-root
 fi
 
